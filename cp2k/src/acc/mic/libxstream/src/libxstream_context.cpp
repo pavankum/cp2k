ef1d6eb536d90d877d2dee6b8f76154f387392a4
Hans Pabst <hans.pabst@intel.com>
2015-02-20 21:16:21 +0100

    Fixed function pointer conversion.

--- cp2k/src/acc/mic/libxstream/src/libxstream_context.cpp
+++ cp2k/src/acc/mic/libxstream/src/libxstream_context.cpp
@@ -33,23 +33,17 @@
 
 libxstream_context& libxstream_context::instance()
 {
-  static LIBXSTREAM_TLS libxstream_context context;
+  LIBXSTREAM_TARGET(mic) static LIBXSTREAM_TLS libxstream_context context;
   return context;
 }
 
 
-libxstream_context& libxstream_context::instance(libxstream_function function, const libxstream_argument arguments[], size_t arity, int flags_)
+libxstream_context& libxstream_context::instance(const libxstream_argument arguments[], size_t arity, int flags_)
 {
   libxstream_context& context = instance();
 
   for (size_t i = 0; i <= arity; ++i) {
     context.signature[i] = arguments[i];
-  }
-  if (0 == (flags_ & LIBXSTREAM_CALL_NATIVE)) {
-    context.fhybrid = function;
-  }
-  else {
-    context.fnative = reinterpret_cast<const void*>(function);
   }
   context.flags = flags_;
 
