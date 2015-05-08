d4de22091e1c3311fe0559fa78e4fef5cc5e94b0
Hans Pabst <hans.pabst@intel.com>
2015-05-08 23:37:14 +0200

    Adjusted for the latest interface of LIBXSTREAM. Improved error handling by asserting correctness where an error is discovered. Removed cumbersome LIBXSTREAM_DEMUX (possible with the latest LIBXSTREAM). Introduced LIBMICACC_NONBLOCKING (environment variable) to allow for non-blocking synchronization (host).

--- cp2k/src/acc/mic/libmicacc.c
+++ cp2k/src/acc/mic/libmicacc.c
@@ -20,19 +20,24 @@ int acc_get_ndevices(int* n_devices)
   size_t ndevices;
   const int result = libxstream_get_ndevices(&ndevices);
   *n_devices = ndevices;
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
   return result;
 }
 
 
 int acc_set_active_device(int device_id)
 {
-  return libxstream_set_active_device(device_id);
+  const int result = libxstream_set_active_device(device_id);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_stream_priority_range(int* least, int* greatest)
 {
-  return libxstream_stream_priority_range(least, greatest);
+  const int result = libxstream_stream_priority_range(least, greatest);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
@@ -40,62 +45,75 @@ int acc_stream_create(void** stream_p, const char* name, int priority)
 {
   int device = -1, result = libxstream_get_active_device(&device);
   LIBXSTREAM_CHECK_ERROR(result);
-#if defined(_OPENMP)
-  const char *const demux_env = getenv("LIBXSTREAM_DEMUX");
-  const int demux = (demux_env && *demux_env) ? atoi(demux_env) : 0;
-#else
-  const int demux = 0;
-#endif
-  result = libxstream_stream_create((libxstream_stream**)stream_p, device, demux, priority, name);
+  result = libxstream_stream_create((libxstream_stream**)stream_p, device, priority, name);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
   return result;
 }
 
 
 int acc_stream_destroy(void* stream)
 {
-  return libxstream_stream_destroy((libxstream_stream*)stream);
+  const int result = libxstream_stream_destroy((libxstream_stream*)stream);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_stream_sync(void* stream)
 {
-  return libxstream_stream_sync((libxstream_stream*)stream);
+  const char *const env = getenv("LIBMICACC_NONBLOCKING");
+  const int non_blocking = (env && *env) ? atoi(env) : 0;
+  const int result = (0 == non_blocking ? libxstream_stream_wait : libxstream_stream_sync)((libxstream_stream*)stream);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_stream_wait_event(void* stream, void* event)
 {
-  return libxstream_stream_wait_event((libxstream_stream*)stream, (libxstream_event*)event);
+  const int result = libxstream_stream_wait_event((libxstream_stream*)stream, (libxstream_event*)event);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_event_create(void** event_p)
 {
-  return libxstream_event_create((libxstream_event**)event_p);
+  const int result = libxstream_event_create((libxstream_event**)event_p);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_event_destroy(void* event)
 {
-  return libxstream_event_destroy((libxstream_event*)event);
+  const int result = libxstream_event_destroy((libxstream_event*)event);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_event_record(void* event, void* stream)
 {
-  return libxstream_event_record((libxstream_event*)event, (libxstream_stream*)stream);
+  const int result = libxstream_event_record((libxstream_event*)event, (libxstream_stream*)stream);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_event_query(void* event, int* has_occured)
 {
-  return libxstream_event_query((libxstream_event*)event, has_occured);
+  const int result = libxstream_event_query((libxstream_event*)event, has_occured);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_event_synchronize(void* event)
 {
-  return libxstream_event_synchronize((libxstream_event*)event);
+  const int result = libxstream_event_wait((libxstream_event*)event);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
@@ -104,6 +122,7 @@ int acc_dev_mem_allocate(void** dev_mem, size_t n)
   int device = -1, result = libxstream_get_active_device(&device);
   LIBXSTREAM_CHECK_ERROR(result);
   result = libxstream_mem_allocate(device, dev_mem, n, 0/*automatic*/);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
   return result;
 }
 
@@ -113,43 +132,56 @@ int acc_dev_mem_deallocate(void* dev_mem)
   int device = -1, result = libxstream_get_active_device(&device);
   LIBXSTREAM_CHECK_ERROR(result);
   result = libxstream_mem_deallocate(device, dev_mem);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
   return result;
 }
 
 
 int acc_host_mem_allocate(void** host_mem, size_t n, void* stream)
 {
-  return libxstream_mem_allocate(-1, host_mem, n, 0);
+  const int result = libxstream_mem_allocate(-1, host_mem, n, 0);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_host_mem_deallocate(void* host_mem, void* stream)
 {
-  return libxstream_mem_deallocate(-1, host_mem);
+  const int result = libxstream_mem_deallocate(-1, host_mem);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_memcpy_h2d(const void* host_mem, void* dev_mem, size_t count, void* stream)
 {
-  return libxstream_memcpy_h2d(host_mem, dev_mem, count, (libxstream_stream*)stream);
+  const int result = libxstream_memcpy_h2d(host_mem, dev_mem, count, (libxstream_stream*)stream);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_memcpy_d2h(const void* dev_mem, void* host_mem, size_t count, void* stream)
 {
-  return libxstream_memcpy_d2h(dev_mem, host_mem, count, (libxstream_stream*)stream);
+  const int result = libxstream_memcpy_d2h(dev_mem, host_mem, count, (libxstream_stream*)stream);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_memcpy_d2d(const void* devmem_src, void* devmem_dst, size_t count, void* stream)
 {
-  return libxstream_memcpy_d2d(devmem_src, devmem_dst, count, (libxstream_stream*)stream);
+  const int result = libxstream_memcpy_d2d(devmem_src, devmem_dst, count, (libxstream_stream*)stream);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
 int acc_memset_zero(void* dev_mem, size_t offset, size_t length, void* stream)
 {
-  return libxstream_memset_zero((char*)dev_mem + offset, length, (libxstream_stream*)stream);
+  const int result = libxstream_memset_zero((char*)dev_mem + offset, length, (libxstream_stream*)stream);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
+  return result;
 }
 
 
@@ -158,6 +190,7 @@ int acc_dev_mem_info(size_t* free, size_t* avail)
   int device = -1, result = libxstream_get_active_device(&device);
   LIBXSTREAM_CHECK_ERROR(result);
   result = libxstream_mem_info(device, free, avail);
+  LIBXSTREAM_ASSERT(LIBXSTREAM_ERROR_NONE == result);
   return result;
 }
 
