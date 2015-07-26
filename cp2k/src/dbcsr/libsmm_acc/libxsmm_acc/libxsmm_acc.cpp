a023737a972e7273fd3ed8fb70deeda99be24c50
Hans Pabst <hans.pabst@intel.com>
2015-07-26 21:49:40 +0200

    Wrap the fully qualified/correct symbol.

--- cp2k/src/dbcsr/libsmm_acc/libxsmm_acc/libxsmm_acc.cpp
+++ cp2k/src/dbcsr/libsmm_acc/libxsmm_acc/libxsmm_acc.cpp
@@ -20,11 +20,11 @@
 
 
 #if defined(__LIBXSMM) && !(defined(__ACC) && defined(__ACC_MIC) && defined(__DBCSR_ACC) && defined(__LIBXSTREAM))
-LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__real_smm_process_mm_stack_s)(const void*, const int*, const int*, const float*, const float*, float*, void*);
-LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__real_smm_process_mm_stack_d)(const void*, const int*, const int*, const double*, const double*, double*, void*);
+LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__real_dbcsr_mm_hostdrv_mp_smm_process_mm_stack_s)(const void*, const int*, const int*, const float*, const float*, float*, void*);
+LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__real_dbcsr_mm_hostdrv_mp_smm_process_mm_stack_d)(const void*, const int*, const int*, const double*, const double*, double*, void*);
 
 
-LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__wrap_smm_process_mm_stack_s)(const void* stack_descr, const int* params, const int* stack_size, const float* a_data, const float* b_data, float* c_data, void* error)
+LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__wrap_dbcsr_mm_hostdrv_mp_smm_process_mm_stack_s)(const void* stack_descr, const int* params, const int* stack_size, const float* a_data, const float* b_data, float* c_data, void* error)
 {
   static const char *const env = getenv("LIBXSMM_ACC_INTERCEPT");
   static const bool intercept = (env && *env) ? (0 != atoi(env)) : false/*default*/;
@@ -36,12 +36,12 @@ LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__wrap_smm_process_mm_stack_s)(con
 #endif
   }
   else { // original code
-    LIBXSMM_ACC_FSYMBOL(__real_smm_process_mm_stack_s)(stack_descr, params, stack_size, a_data, b_data, c_data, error);
+    LIBXSMM_ACC_FSYMBOL(__real_dbcsr_mm_hostdrv_mp_smm_process_mm_stack_s)(stack_descr, params, stack_size, a_data, b_data, c_data, error);
   }
 }
 
 
-LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__wrap_smm_process_mm_stack_d)(const void* stack_descr, const int* params, const int* stack_size, const double* a_data, const double* b_data, double* c_data, void* error)
+LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__wrap_dbcsr_mm_hostdrv_mp_smm_process_mm_stack_d)(const void* stack_descr, const int* params, const int* stack_size, const double* a_data, const double* b_data, double* c_data, void* error)
 {
   static const char *const env = getenv("LIBXSMM_ACC_INTERCEPT");
   static const bool intercept = (env && *env) ? (0 != atoi(env)) : false/*default*/;
@@ -50,7 +50,7 @@ LIBXSMM_ACC_EXTERN_C void LIBXSMM_ACC_FSYMBOL(__wrap_smm_process_mm_stack_d)(con
     // TODO
   }
   else { // original code
-    LIBXSMM_ACC_FSYMBOL(__real_smm_process_mm_stack_d)(stack_descr, params, stack_size, a_data, b_data, c_data, error);
+    LIBXSMM_ACC_FSYMBOL(__real_dbcsr_mm_hostdrv_mp_smm_process_mm_stack_d)(stack_descr, params, stack_size, a_data, b_data, c_data, error);
   }
 }
 #endif
