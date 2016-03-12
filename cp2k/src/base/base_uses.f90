72d9945e6856fa429a2d1bb14debcf03a91c7a66
Hans Pabst <hans.pabst@intel.com>
2016-03-12 12:36:51 +0100

    Introduced CPFVERSION3 macro which helps calculating a single version number out of a three-component version (major, minor, update). The CPFVERSION3 macro is helpful when writing correct preprocessor conditions for code depending on the compiler version.
    Introduced the CPCONTIGUOUS macro which may (or may not) expand to the CONTIGUOUS attribute depending on whether or not the compiler supports the Fortran 2008 standard. The CONTIGUOUS macro helps vectorizing code which is accessing POINTER-based arrays (implying unit-stride rather than an unknown stride).

--- cp2k/src/base/base_uses.f90
+++ cp2k/src/base/base_uses.f90
@@ -27,3 +27,18 @@
 ! This code should be valid for any Fortran variable, is always standard conforming,
 ! and will be optimized away completely by the compiler
 #define MARK_USED(foo) IF(.FALSE.)THEN; DO ; IF(SIZE(SHAPE(foo))==-1) EXIT ;  END DO ; ENDIF
+
+! Macro which is calculating a single version number out of three components (major, minor, update).
+! This is helpful when writing preprocessor conditions for code depending on the compiler version.
+#define CPFVERSION3(MAJOR, MINOR, UPDATE) (MAJOR * 10000 + MINOR * 100 + UPDATE)
+
+! The CPCONTIGUOUS macro may (or may not) expand to the CONTIGUOUS attribute
+! depending on whether or not the compiler supports Fortran 2008.
+#if defined(__GFORTRAN__) && (40600 <= CPFVERSION3(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__))
+# define CPCONTIGUOUS CONTIGUOUS
+#elif defined(__INTEL_COMPILER) && (1210 <= __INTEL_COMPILER)
+# define CPCONTIGUOUS CONTIGUOUS
+#else
+# define CPCONTIGUOUS
+#endif
+
